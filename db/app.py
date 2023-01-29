aed41b5a05581125dfa2892e47966981a697b422
t2tran <15519+t2tran@users.noreply.github.tamu.edu>
2023-01-28 22:04:53 -0600

    everything in app now

--- db/app.py
+++ db/app.py
@@ -1,18 +1,18 @@
-# from flask import Flask
-# import db
-# app = Flask(__name__)
-# @app.route('/')
-# def flask_mongodb_atlas():
-#     return "flask mongodb atlas!"
-
-# #test to insert data to the data base
-# @app.route("/test")
-# def test():
-#     db.db.collection.insert_one({"name": "John"})
-#     return "Connected to the data base!"
 from flask import Flask
-app = Flask(__name__)
+from flask_pymongo import pymongo
 
+CONNECTION_STRING = "mongodb+srv://kaster-admin:dutyland@kaster.gbdbqkw.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('kaster_users')
+user_collection = pymongo.collection.Collection(db, 'user_collection')
+
+app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def flask_mongodb_atlas():
+    return "flask mongodb atlas!"
+
+#test to insert data to the data base
+@app.route('/test')
+def test():
+    db.db.collection.insert_one({"name": "John"})
+    return "Connected to the data base!"
--- db/db.py
+++ db/db.py
@@ -1,10 +1,4 @@
-from flask import Flask
-from flask_pymongo import pymongo
-from app import app
-CONNECTION_STRING = "mongodb+srv://kaster-admin:dutyland@kaster.gbdbqkw.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('kaster_users')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
+
 
 # from flask import Flask
 # from pymongo import MongoClient
