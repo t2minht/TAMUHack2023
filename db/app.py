672d67766174e9b1afc66393abc7396d745c4d98
t2tran <15519+t2tran@users.noreply.github.tamu.edu>
2023-01-28 22:00:25 -0600

    dummy code for app.py

--- db/app.py
+++ db/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
-import db
-app = Flask(__name__)
-@app.route('/')
-def flask_mongodb_atlas():
-    return "flask mongodb atlas!"
+# from flask import Flask
+# import db
+# app = Flask(__name__)
+# @app.route('/')
+# def flask_mongodb_atlas():
+#     return "flask mongodb atlas!"
 
-#test to insert data to the data base
-@app.route("/test")
-def test():
-    db.db.collection.insert_one({"name": "John"})
-    return "Connected to the data base!"
+# #test to insert data to the data base
+# @app.route("/test")
+# def test():
+#     db.db.collection.insert_one({"name": "John"})
+#     return "Connected to the data base!"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
